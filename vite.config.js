95af16d41bcc7ec441601c10f6614dd964dad53d
masafumi yamaguchi <karubijyukou@gmail.com>
2025-03-16 10:08:42 +0900

    Update asset copy configuration to target specific files in Vite

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: "src/assets",
+          src: "src/assets/*",
           dest: "assets",
         },
       ],
